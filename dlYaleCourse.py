5a041bfbff397b224b7832d8aaeb41f9242189a5
Brian Johnson <brian@sherbang.com>
2015-08-22 20:04:25 -0400

    Rework for simplicity and python3 support

--- dlYaleCourse.py
+++ dlYaleCourse.py
@@ -1,43 +1,36 @@
-#!/usr/bin/python
-import httplib
+#!/usr/bin/env python3
 import re
 import sys
+from robobrowser import RoboBrowser
+from script_helpers.urllib import FileExistsException, download_file
+from urllib.parse import urljoin
 
-sessions_page = sys.argv[1]
 
-oyc = 'oyc.yale.edu'; 
-openmedia = 'openmedia.yale.edu'
+def get_mp3_url(lecture_url):
+    browser = RoboBrowser()
+    browser.open(lecture_url)
 
-def readPage(domain, pageURL, headers=None):
-    cxn = httplib.HTTPConnection(domain)
-    if headers: 
-        cxn.request('GET', pageURL, headers=headers)
-    else:
-        cxn.request('GET', pageURL)
-    resp = cxn.getresponse()
-    cxn.close();
-    page = resp.read()
-    return page
+    return browser.get_link(href=re.compile('\\.mp3$'))['href']
 
-course = readPage(oyc, sessions_page)
-lectureURLs = re.findall("href=\"(.*lecture.*)\"", course)
 
-for lectureURL in lectureURLs:
-    lecture = readPage(oyc, lectureURL)
-    mp3URLs = re.findall("href=\"([^\"]*?mp3)\"", lecture)
-    for mp3FullURL in mp3URLs:
-        print mp3FullURL
-        if(re.match(".*yale\.edu.*", mp3FullURL) == None):
-            print "Not downloading"
-            continue
-        m = re.match(".*file=.*/(.*?\.mp3)", mp3FullURL)
-        mp3FileName =  m.group(1)
-        
-        m = re.match(".*yale.edu(/.*?\.mp3)", mp3FullURL)
-        mp3URL =  m.group(1)
-        print "reading %s" % mp3URL
-        headers = {'Referer':"http://" + oyc + lectureURL}
-        mp3 = readPage(openmedia, mp3URL, headers)
-        mp3File = file(mp3FileName, "w")
-        mp3File.write(mp3)
-        print "writing to  %s" % mp3FileName
+if __name__ == '__main__':
+    sessions_page = sys.argv[1]
+
+    browser = RoboBrowser()
+    browser.open(sessions_page)
+
+    lecture_links = browser.get_links(href=re.compile('lecture-'))
+
+    for link in lecture_links:
+        lecture_url = link['href']
+        # ensure absolute url:
+        lecture_url = urljoin(sessions_page, lecture_url)
+
+        mp3_url = get_mp3_url(lecture_url)
+
+        try:
+            filename = download_file(mp3_url)
+            print('Downloaded {}'.format(filename))
+        except FileExistsException as exc:
+            filename = exc.filename
+            print('File ({}) already exists - skipped'.format(filename))
